9435744a9cb2d4441ad75848735ea4a046a4a348
Afam <afam262@gmail.com>
2020-11-17 01:18:11 -0600

    adding html, css and script

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+$('#searchBtn').on('click', function(){
+    let userInput = $('#city').val();
+    let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=a19feae6aba79b23ca90858cd79bfdb3';
+})
