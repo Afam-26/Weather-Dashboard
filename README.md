509e6d50d6e5c0aae16074935a6c96bfad7368e1
Afam <afam262@gmail.com>
2020-11-22 14:05:43 -0600

    add readme, updating js and css

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-Dashboard
+
+This is a Weather Dashboard that uses third-party APIs to retrieve data and use it in the context of its own. This application have the ability to display the weather outlook of multiple cities at a time. When a user search for a city, the user is presented with current and future conditions for that city and that city is added to the search history. When a user view current weather conditions for that city, the user is presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index. When a user view future weather conditions for that city, the user is resented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity.
+
+## Screen shot
+![Front page](./assets/front.png)
+
+
+## Link to application
+
+[Click here for link](https://afam-26.github.io/Weather-Dashboard/)
+
+## credit
+
+Brennan Predmore TA
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    const myKey = '3378373f3ab215405fc8140860945152'
+    
 $('#searchBtn').click(function () {
   let userInput = $('#city').val();
-  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${myKey}`;
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=a19feae6aba79b23ca90858cd79bfdb3`;
   //AJAX CALL FOR CURRENT WEATHER
   $.ajax({
     url: queryURL,
@@ -32,3 +32,40 @@ $('#searchBtn').click(function () {
   });
 });
 
+function getForcast(input) {
+    let fiveDayQueryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${input}&units=imperial&appid=a19feae6aba79b23ca90858cd79bfdb3`;
+    // AJAX CALL FOR FIVE DAY FORECAST
+    $.ajax({
+      url: fiveDayQueryURL,
+      type: 'GET',
+    }).then(function (response) {
+      $('#forecast')
+        .html('<h4 class="mt-3">5-Day Forecast:</h4>')
+        .append('<div class="row">');
+      console.log(response);
+      for (let i = 0; i < response.list.length; i++) {
+        let hour = response.list[i];
+        if (hour.dt_txt.indexOf('00:00:00') != -1) {
+          let date = new Date(hour.dt_txt).toLocaleDateString();
+          hour.main.temp;
+          hour.main.humidity;
+          let DIV = $('<span>');
+          let image = $('<img>');
+          image.attr(
+            'src',
+            `http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`
+          );
+          DIV.addClass('divClasses');
+          DIV.append(`<h3>${date}</h3>`);
+          DIV.append(image);
+          DIV.append(
+            `<p>Temperture <br>  ${
+              hour.main.temp + 'º F'
+            } </p><p>Humidity <br> ${hour.main.humidity + '%'}</p>`
+          );
+          $('#fiveDayForecast').append(DIV);
+        }
+      }
+    });
+  }
+});
